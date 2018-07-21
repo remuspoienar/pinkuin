bae4c6adf5192cd2478d5b0c73e2cf9ed60fa4eb
Remus Poienar <remuspoienar@yahoo.com>
2018-07-21 22:13:05 +0300

    Use seeds only on development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
+# require 'faker'
 
-10.times do
-  user = User.new(username: Faker::Internet.user_name,
-                  email:    Faker::Internet.email,
-                  password: Faker::Internet.password(6, 12))
-  user.skip_confirmation!
-  user.save
-
-  2.times do
-    user.projects.create!(name:        Faker::Company.name,
-                          description: Faker::Lorem.paragraph(3),
-                          status:      Project::STATUS_INACTIVE,
-                          api_host:    Faker::Internet.url,
-                          properties:  { a: 1, b: 2 })
-  end
-end
+# 10.times do
+#   user = User.new(username: Faker::Internet.user_name,
+#                   email:    Faker::Internet.email,
+#                   password: Faker::Internet.password(6, 12))
+#   user.skip_confirmation!
+#   user.save
+#
+#   2.times do
+#     user.projects.create!(name:        Faker::Company.name,
+#                           description: Faker::Lorem.paragraph(3),
+#                           status:      Project::STATUS_INACTIVE,
+#                           api_host:    Faker::Internet.url,
+#                           properties:  { a: 1, b: 2 })
+#   end
+# end
